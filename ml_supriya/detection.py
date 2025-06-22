2b12808f8c2e0d2443d2279cf3ca08ba677d38e2
Sushma Shrestha <sushma.sw14@gmail.com>
2025-06-22 13:08:37 +0545

    updated with conf

--- ml_supriya/detection.py
+++ ml_supriya/detection.py
@@ -22,7 +22,7 @@ def detect_objects():
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     
     # Run detection
-    results = model(image)
+    results = model(image) 
     
     # Extract classes
     detected_classes = []
